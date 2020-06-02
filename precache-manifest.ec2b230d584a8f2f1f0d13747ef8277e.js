ad4e34537cb61814f890fa0b901011101915cef2
levtsypanov <levdark7@gmail.com>
2020-06-02 16:46:33 +0300

    Init 🚀🚀

---
+++ precache-manifest.ec2b230d584a8f2f1f0d13747ef8277e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5d44e0c1f546c74957b2f1dd6f116b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6264e148123bce1c262",
+    "url": "/static/css/2.814df5ce.chunk.css"
+  },
+  {
+    "revision": "c6264e148123bce1c262",
+    "url": "/static/js/2.f12ca387.chunk.js"
+  },
+  {
+    "revision": "b1c79da2ad8a1be65df9",
+    "url": "/static/js/main.66066022.chunk.js"
+  },
+  {
+    "revision": "962481667bdc1ff4df96",
+    "url": "/static/js/runtime-main.4bf4f9c4.js"
+  }
+]);
